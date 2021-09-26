271f745fe45c139bc39dcd41d908baaa27b2a705
Prajwal Shreshta Grandhi <70463518+PrajwalGrandhi@users.noreply.github.com>
2021-09-26 12:26:05 +0530

    Add files via upload

---
+++ bs.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import time
+import keyboard
+fourcc=cv2.VideoWriter_fourcc(*'XVID')
+output_file=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+
+cap=cv2.VideoCapture(0)
+image=cv2.imread('bangkok.jpg')
+while(cap.isOpened()):
+      ret,frame = cap.read()
+      print(frame)
+      frame=cv2.resize(frame,(640,480))
+      image=cv2.resize(image,(640,480))
+
+      u_black=np.array([104,153,70])
+      l_black=np.array([30,30,0])
+
+      mask=cv2.inRange(frame,l_black,u_black)
+      res=cv2.bitwise_and(frame,frame,mask=mask)
+
+      f=frame-res
+      f=np.where(f==0,image,f)
+
+      #cv2.imshow("video", frame) 
+      cv2.imshow("mask", f) 
+
+      output_file.write(f)
+      if cv2.waitKey(1)&keyboard.is_pressed("q"):
+        break
